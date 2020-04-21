c7f6e7ce57cfe0acda15406b745e2a154774efe8
SuperNoice <SuperNoice@yandex.ru>
2020-04-21 13:47:52 +0300

    individual_1 v0.6

---
+++ individual_1.pl
@@ -0,0 +1,10 @@
+num(Fr,To,X):-ToN is To+1, num2(Fr,ToN,X).
+num2(Fr,To,X):-FrN is Fr+1, FrN<To, num2(FrN,To,X).
+num2(X,_,X).
+
+st(Os,St,X):-st2(Os,St,X,Os,1).
+st2(_,0,1,_,_):-!.
+st2(_,St,R,R,St):-!.
+st2(Os,St,X,R,I):-I<St, RN is R*Os, IN is I+1, st2(Os,St,X,RN,IN).
+
+pr(Os,X):-num(0,3,St),st(Os,St,X).
