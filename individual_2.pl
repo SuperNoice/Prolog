75fe78589359247ea4b430a78cb2e9c9873a54ec
SuperNoice <SuperNoice@yandex.ru>
2020-04-21 21:17:31 +0300

    individual_2 v1.0

--- individual_2.pl
+++ individual_2.pl
@@ -9,3 +9,11 @@ count_el(El,List,Count):-count_el(El,List,Count,0).
 count_el(_,[],Count,Count):-!.
 count_el(El,[El|T],Count,Cur):-Cur1 is Cur+1, count_el(El,T,Count,Cur1),!.
 count_el(El,[_|T],Count,Cur):-count_el(El,T,Count,Cur).
+
+writeList([]):-!.
+writeList([H|T]):-write(H),write(" "),writeList(T).
+
+main(L):-uniq_el(L,L1), writeList(L1),nl, kolinit(L,L1,L2), writeList(L2).
+kolinit(L,L1,L2):-kol(L,L1,L2,[]).
+kol(_,[],L2,L2):-!.
+kol(L,[H|T],L2,Tmp):-count_el(H,L,C), append(Tmp,[C],L2new), kol(L,T,L2,L2new).
