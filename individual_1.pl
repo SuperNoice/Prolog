37b6328a7e3578512f6eb9461e85d51060cd6c59
SuperNoice <SuperNoice@yandex.ru>
2020-04-21 20:27:05 +0300

    individual_1 v1.0

--- individual_1.pl
+++ individual_1.pl
@@ -7,6 +7,8 @@ st2(_,0,1,_,_):-!.
 st2(_,St,R,R,St):-!.
 st2(Os,St,X,R,I):-I<St, RN is R*Os, IN is I+1, st2(Os,St,X,RN,IN).
 
-all_st(X):-num(2,5,St),num(2,5,Os),st(Os,St,X).
+all_st(X,N,M):-num(2,N,Os),num(2,M,St),st(Os,St,X).
 
-
+unic_count(_,N,M):-asserta(count(0)), asserta(mem(0)),asserta(i(0)), all_st(A,N,M), uc(A), fail.
+unic_count(X,_,_):-!,count(X),!, retractall(mem(_)), retractall(count(_)).
+uc(A):-not(mem(A)), asserta(mem(A)), count(C),!, NewI is C+1, asserta(count(NewI)).
