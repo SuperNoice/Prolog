4537c2e0fdb9ec075d99d5ae8e01f54b0e8123aa
SuperNoice <SuperNoice@yandex.ru>
2020-04-23 00:26:07 +0300

    individual_9 v1.0

---
+++ individual_9.fs
@@ -0,0 +1,43 @@
+﻿let prost x =
+    let mutable k=0
+    let mutable i=1
+    while (i<=x) do 
+        if (x%i=0) then
+            k<-k+1
+        i<-i+1
+    if (k=2) then true
+    else false
+    
+let start() =
+    let mutable max=0
+    let mutable count=0
+    let mutable n=0
+    let mutable memA=0
+    let mutable memB=0
+    for a in -999 .. 999 do
+        for b in -999 .. 999 do
+            count<-0
+            n<-0
+            while prost (n*n+a*n+b) do
+                count<-count+1
+                n<-n+1
+            if (count>max) then 
+                max<-count
+                memA<-a
+                memB<-b
+    memA*memB
+
+let test() =
+    let mutable count=0
+    let mutable n=0
+    while prost (n*n+n+41) do
+        count<-count+1
+        n<-n+1
+    count
+    
+
+[<EntryPoint>]
+let main argv =
+    System.Console.WriteLine(start())
+    System.Console.ReadKey()
+    0
